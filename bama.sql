b292e87ccbe274aeb0ae9e3cb856ef9204209563
Zach <zachary.fey@gmail.com>
2019-04-25 20:00:28 -0400

    Created database and table. Established connection to table with NodeJS and listed available items

---
+++ bama.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'zachary';
+USE bamazon;
+
+CREATE TABLE products(
+	item_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    product_name VARCHAR(50) NOT NULL,
+    department_name VARCHAR(20) NOT NULL,
+    price INTEGER NOT NULL,
+    stock_quantity INTEGER DEFAULT 0
+	);
+    
+    SELECT * FROM products;
+
+DELETE FROM products WHERE product_name = 'television';
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('television', 'electronics', 30000, 7);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('bananas', 'produce', 30, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('helmet', 'sports', 2000, 15);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('beer', 'groceries', 1000, 40);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('gum', 'groceries', 1000, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('shoes', 'clothing', 5000, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('apples', 'produce', 80, 18);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('laptop', 'electronics', 50000, 10);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('bicycle', 'sports', 30000, 5);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('shirt', 'clothing', 2000, 10);
+
+SELECT * FROM products;
