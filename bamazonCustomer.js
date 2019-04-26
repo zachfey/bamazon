40cdbb9572c49d2f848bd78ca3fbc72cf03cc3ba
Zach <zachary.fey@gmail.com>
2019-04-25 20:14:12 -0400

    responds to user based on selection and desired quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,28 +14,38 @@ connection.connect(function(err){
     console.log('connected!\n------------------------------------------------------');
     connection.query('SELECT * FROM products', function(err, res){
         if(err) throw err;
+
         console.log('Welcome to Bamazon!\n\nWe have the following items for sale:\n');
+        
         for (let i in res){
             console.log('Item ' + res[i].item_id + ': ' + res[i].product_name +' for only $' + (res[i].price/100).toFixed(2) + ' each. Act fast, only ' + res[i].stock_quantity + ' remain!');
         }
 
         inquirer.prompt([{
-            message: 'Which item would you like to buy?',
+            message: 'Which item would you like to buy? (Please enter the item ID)',
             type: 'prompt',
             name: 'itemID'
-        }]).then(function(answers){
-            console.log(answers.itemID);
+        }, 
+        {
+            message: 'How many would you like?',
+            name: 'quantity'
+        }
+    ]).then(function(answers){
+            console.log('OK, buying ' + answers.quantity + ' of item ' + answers.itemID);
+            connection.query('SELECT stock_quantity, product_name FROM products WHERE item_id = ?', [answers.itemID], function(err, res){
+                if(err) throw err;
+                console.log(res[0]);
+                const stock = res[0].stock_quantity;
+                const item = res[0].product_name;
+                if(answers.quantity > stock){
+                    console.log("Sorry, we only have " + stock + ' ' + item + 's');
+                }
+                connection.end();
+
+            });
         })
     })
-    connection.end();
 })
-
-// The app should then prompt users with two messages.
-
-// The first should ask them the ID of the product they would like to buy.
-// The second message should ask how many units of the product they would like to buy.
-
-
 
 // Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
 
